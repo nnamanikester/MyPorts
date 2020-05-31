143e752a10c294904775b52779c993d165e3b832
kescript <nnamanikester@gmail.com>
2020-05-31 20:23:56 +0100

    Created a loading component

---
+++ src/components/common/Loading/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {TouchableOpacity, StyleSheet} from 'react-native';
+import {Text, Layout} from '../';
+
+const Loading = () => {
+  return (
+    <>
+      <Layout>
+        <Text>Loading</Text>
+      </Layout>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default Loading;
