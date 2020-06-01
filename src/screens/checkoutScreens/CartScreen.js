5efbf0dbc2ea2cc2ff229e827bd13364b417dbe1
kescript <nnamanikester@gmail.com>
2020-06-01 18:19:53 +0100

    Designed the Cart Screen

--- src/screens/checkoutScreens/CartScreen.js
+++ src/screens/checkoutScreens/CartScreen.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import {StyleSheet, View, TouchableOpacity} from 'react-native';
-import {Text, Layout, Icon, ListItem, Spacer} from '../../components/common';
+import {
+  Text,
+  Layout,
+  Icon,
+  ListItem,
+  Spacer,
+  TextInput,
+  Row,
+  Button,
+  Divider,
+} from '../../components/common';
 import Header from '../../components/Header';
 import Avater from '../../components/Avatar';
-import {female4} from '../../assets/images';
+import {female4, female1, female2} from '../../assets/images';
+import {info} from '../../components/common/variables';
 
 const CartScreen = ({navigation}) => {
   return (
@@ -28,24 +39,113 @@ const CartScreen = ({navigation}) => {
         }
       />
       <Layout>
+        <Spacer medium />
         <ListItem
-          left={<Avater src={female4} />}
+          left={<Avater src={female4} size={100} />}
           body={
             <>
-              <Text heading>Leather Bag</Text>
+              <Text heading>Leather Show Bag</Text>
               <Spacer />
-              <Text note>Color</Text>
-              <Text note>Size</Text>
-              <Text note>Quantity</Text>
+              <Text>
+                Size: <Text color={info}>XL</Text>
+              </Text>
+              <Text>
+                Color: <Text color={info}>Red</Text>
+              </Text>
+              <Text>
+                Quantity: <Text color={info}>24</Text>
+              </Text>
             </>
           }
           right={<Text>NGN 23,500</Text>}
         />
+        <ListItem
+          left={<Avater src={female2} size={100} />}
+          body={
+            <>
+              <Text heading>Leather Show Bag</Text>
+              <Spacer />
+              <Text>
+                Size: <Text color={info}>XL</Text>
+              </Text>
+              <Text>
+                Color: <Text color={info}>Red</Text>
+              </Text>
+              <Text>
+                Quantity: <Text color={info}>24</Text>
+              </Text>
+            </>
+          }
+          right={<Text>NGN 23,500</Text>}
+        />
+        <ListItem
+          left={<Avater src={female1} size={100} />}
+          body={
+            <>
+              <Text heading>Leather Show Bag</Text>
+              <Spacer />
+              <Text>
+                Size: <Text color={info}>XL</Text>
+              </Text>
+              <Text>
+                Color: <Text color={info}>Red</Text>
+              </Text>
+              <Text>
+                Quantity: <Text color={info}>24</Text>
+              </Text>
+            </>
+          }
+          right={<Text>NGN 23,500</Text>}
+        />
+
+        <Spacer medium />
+
+        <View style={styles.container}>
+          <Text heading>Enter your coupon code here</Text>
+
+          <Spacer />
+
+          <TextInput placeholder="Coupon Code" />
+
+          <Spacer medium />
+
+          <Row style={{justifyContent: 'space-between'}}>
+            <Text heading>Order: </Text>
+            <Text>NGN 63,000</Text>
+          </Row>
+
+          <Spacer />
+
+          <Row style={{justifyContent: 'space-between'}}>
+            <Text heading>Shipping: </Text>
+            <Text>NGN 3,000</Text>
+          </Row>
+
+          <Spacer />
+          <Divider />
+
+          <Row style={{justifyContent: 'space-between'}}>
+            <Text bold>Total: </Text>
+            <Text bold>NGN 66,000</Text>
+          </Row>
+
+          <Spacer large />
+
+          <Button>
+            <Text color="#fff">Place Order</Text>
+          </Button>
+
+          <Spacer large />
+        </View>
       </Layout>
     </>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 10,
+  },
+});
 
 export default CartScreen;
