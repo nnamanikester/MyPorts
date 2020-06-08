1ac66303b64c28bc0da7afa2493ee63c8488ec60
kescript <nnamanikester@gmail.com>
2020-06-08 01:24:58 +0100

    Imported all the downloaded images

--- src/assets/images.js
+++ src/assets/images.js
@@ -1,10 +1,77 @@
-import female1 from './images/Img-Female01.png';
-import female2 from './images/Img-Female22.png';
-import female3 from './images/Img-Female28.png';
-import male1 from './images/Img-Male-08.png';
-import bag1 from './images/bag1.png';
-import shoe1 from './images/show1.png';
-import shoe2 from './images/show2.png';
-import female4 from './images/female4.png';
+import female1 from './images/image1.jpg';
+import female2 from './images/image2.jpg';
+import female3 from './images/image10.jpg';
+import male1 from './images/image9.jpg';
+import bag1 from './images/image8.jpg';
+import shoe1 from './images/image7.jpg';
+import shoe2 from './images/image3.jpg';
+import female4 from './images/image4.jpg';
+import image1 from './images/image1.jpg';
+import image2 from './images/image2.jpg';
+import image3 from './images/image3.jpg';
+import image4 from './images/image4.jpg';
+import image5 from './images/image5.jpg';
+import image6 from './images/image6.jpg';
+import image7 from './images/image7.jpg';
+import image8 from './images/image8.jpg';
+import image9 from './images/image9.jpg';
+import image10 from './images/image10.jpg';
+import food1 from './images/food1.jpg';
+import food2 from './images/food2.jpg';
+import food3 from './images/food3.jpg';
+import food4 from './images/food4.jpg';
+import food5 from './images/food5.jpg';
+import food6 from './images/food6.jpg';
+import food7 from './images/food7.jpg';
+import food8 from './images/food8.jpg';
+import cloth1 from './images/coth2.jpg';
+import cloth2 from './images/cloth3.jpg';
+import cloth3 from './images/cloth4.jpg';
+import cloth4 from './images/cloth5.jpg';
+import cloth5 from './images/cloth6.jpg';
+import cloth6 from './images/cloth7.jpg';
+import shoe3 from './images/shoe1.jpg';
+import shoe4 from './images/shoe2.jpg';
+import shoe5 from './images/shoe3.jpg';
+import shoe6 from './images/shoe4.jpg';
+import shoe7 from './images/shoe5.jpg';
 
-export {female1, female2, female3, female4, male1, bag1, shoe1, shoe2};
+export {
+  female1,
+  female2,
+  female3,
+  female4,
+  male1,
+  bag1,
+  shoe1,
+  shoe2,
+  shoe3,
+  shoe4,
+  shoe5,
+  shoe6,
+  shoe7,
+  image1,
+  image2,
+  image3,
+  image4,
+  image5,
+  image6,
+  image7,
+  image8,
+  image9,
+  image10,
+  food1,
+  food2,
+  food3,
+  food4,
+  food5,
+  food6,
+  food7,
+  food8,
+  cloth1,
+  cloth2,
+  cloth3,
+  cloth4,
+  cloth5,
+  cloth6,
+};
