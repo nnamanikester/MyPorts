b74c26cc4dd3743add446d6fd23ba14669afcea4
kescript <nnamanikester@gmail.com>
2020-06-12 21:05:05 +0100

    Created VDConversation Screen

---
+++ src/screens/VDScreens/VDConversationScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, Layout } from '../../components/common';
+
+const VDConversationScreen = () => {
+  return (
+    <>
+      <Layout>
+        <Text>Conversation Screen</Text>
+      </Layout>
+    </>
+  );
+};
+
+export default VDConversationScreen;
