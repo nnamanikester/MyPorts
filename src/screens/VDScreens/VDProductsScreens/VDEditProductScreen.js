91abde03d968d6d70a527bdb74cccfebbdc957dc
kescript <nnamanikester@gmail.com>
2020-06-12 23:47:35 +0100

    Scaffolded the EditProductScreen

---
+++ src/screens/VDScreens/VDProductsScreens/VDEditProductScreen.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Text } from '../../../components/common';
+import { View, StyleSheet } from 'react-native';
+
+const VDEditProductScreen = () => {
+  return (
+    <>
+      <Layout>
+        <Text>Edit products screen</Text>
+      </Layout>
+    </>
+  );
+};
+
+export default VDEditProductScreen;
