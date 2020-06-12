721de00be8c3edc06b3776db89b2c082686bd656
kescript <nnamanikester@gmail.com>
2020-06-12 23:46:50 +0100

    Scaffolded the AddProduct screen

---
+++ src/screens/VDScreens/VDProductsScreens/VDAddProductScreen.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Text } from '../../../components/common';
+import { View, StyleSheet } from 'react-native';
+
+const VDAddProductScreen = () => {
+  return (
+    <>
+      <Layout>
+        <Text>Add products screen</Text>
+      </Layout>
+    </>
+  );
+};
+
+export default VDAddProductScreen;
