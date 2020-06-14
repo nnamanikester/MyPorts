de9780c4daba31ab190d73b579c8fe859d5f0c24
kescript <nnamanikester@gmail.com>
2020-06-14 20:08:10 +0100

    Designed the conversationScreen Header

--- src/screens/VDScreens/VDConversationScreen.js
+++ src/screens/VDScreens/VDConversationScreen.js
@@ -1,14 +1,38 @@
 import React from 'react';
-import { Text, Layout } from '../../components/common';
+import {
+  Text,
+  Layout,
+  Spacer,
+  Clickable,
+  Icon,
+  Avatar,
+} from '../../components/common';
+import { View, StyleSheet, Image } from 'react-native';
+import { primaryColor, grayColor } from '../../components/common/variables';
+import Header from '../../components/Header';
+import { food3 } from '../../assets/images';
 
 const VDConversationScreen = () => {
   return (
     <>
-      <Layout>
-        <Text>Conversation Screen</Text>
-      </Layout>
+      <Header
+        title="John Kester"
+        headerLeft={
+          <Clickable
+            style={{ flexDirection: 'row', alignItems: 'center' }}
+            onClick={() => navigation.goBack()}>
+            <Icon name="ios-arrow-back" color="#fff" />
+            <Spacer />
+            <Avatar src={food3} rounded />
+          </Clickable>
+        }
+        headerRight={<View></View>}
+      />
+      <Layout></Layout>
     </>
   );
 };
+
+const styles = StyleSheet.create({});
 
 export default VDConversationScreen;
