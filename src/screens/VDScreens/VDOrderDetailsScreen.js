15f91053052fc0c1f52ab6b22f9f60e8fb618f66
kescript <nnamanikester@gmail.com>
2020-06-15 20:53:02 +0100

    Boilerplated the screen|

---
+++ src/screens/VDScreens/VDOrderDetailsScreen.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import { Text, Layout } from '../../components/common';
+
+const VDOrderDetailsScreen = () => {
+  return (
+    <View>
+      <Layout></Layout>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default VDOrderDetailsScreen;
