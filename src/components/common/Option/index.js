3c6d54baf9550c9010d56ff18afc2ce8000ecb7d
kescript <nnamanikester@gmail.com>
2020-06-15 20:49:52 +0100

    Designed the orders Screen

--- src/components/common/Option/index.js
+++ src/components/common/Option/index.js
@@ -1,27 +1,18 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
+// import ModalDropdown from 'react-native-modal-dropdown';
 import { Text } from '../Text';
 
-const Option = () => {
-  return (
-    <View>
-      <View style={styles.container}></View>
-    </View>
-  );
+const Option = ({ options, children }) => {
+  // return (
+  // <ModalDropdown options={options}>
+  //   {children ? children : null}
+  // </ModalDropdown>
+  // );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    elevation: 10,
-    minWidth: 150,
-    overflow: 'scroll',
-    backgroundColor: '#fff',
-    position: 'absolute',
-    right: 0,
-    padding: 10,
-    borderRadius: 5,
-    justifyContent: 'space-between',
-  },
+  container: {},
 });
 
 export { Option };
--- src/screens/VDScreens/VDDashboardScreens/VDOrdersScreen.js
+++ src/screens/VDScreens/VDDashboardScreens/VDOrdersScreen.js
@@ -1,14 +1,113 @@
 import React from 'react';
-import { Text, Layout } from '../../../components/common';
+import {
+  Text,
+  Layout,
+  Icon,
+  ListItem,
+  Avatar,
+  Spacer,
+} from '../../../components/common';
+import { View, StyleSheet } from 'react-native';
+import {
+  primaryColor,
+  success,
+  danger,
+  warning,
+  info,
+} from '../../../components/common/variables';
+import { food3 } from '../../../assets/images';
 
-const VDAccountSettingsScreen = () => {
+const VDAccountSettingsScreen = ({ navigation }) => {
   return (
     <>
       <Layout>
-        <Text>Account Settings screen</Text>
+        <Spacer />
+        <ListItem
+          marked
+          onClick={() => navigation.navigate('VDConversation')}
+          left={<Avatar medium rounded src={food3} />}
+          body={
+            <>
+              <Text heading>Order No 9875934734</Text>
+              <Text numberOfLines={1} color="" note>
+                Enugu, John Kester.
+              </Text>
+            </>
+          }
+          right={
+            <View style={{ justifyContent: 'center', flex: 1 }}>
+              <Text color={success} note style={styles.status}>
+                New
+              </Text>
+            </View>
+          }
+        />
+        <Spacer />
+        <ListItem
+          onClick={() => navigation.navigate('VDConversation')}
+          left={<Avatar medium rounded src={food3} />}
+          body={
+            <>
+              <Text
+                color={info}
+                style={{ textDecoration: 'line-through' }}
+                heading>
+                Order No 9875934734
+              </Text>
+              <Text numberOfLines={1} color="" note>
+                Benin, Ben Kennedy.
+              </Text>
+            </>
+          }
+          right={
+            <View style={{ justifyContent: 'center', flex: 1 }}>
+              <Text color={danger} note style={styles.status}>
+                Cancelled
+              </Text>
+            </View>
+          }
+        />
+        <Spacer />
+        <ListItem
+          onClick={() => navigation.navigate('VDConversation')}
+          left={<Avatar medium rounded src={food3} />}
+          body={
+            <>
+              <Text color={info} heading>
+                Order No 9875934734
+              </Text>
+              <Text numberOfLines={1} color="" note>
+                Lagos, Mary Chucks.
+              </Text>
+            </>
+          }
+          right={
+            <View style={{ justifyContent: 'center', flex: 1 }}>
+              <Text color="" note style={styles.status}>
+                Delivered
+              </Text>
+            </View>
+          }
+        />
       </Layout>
     </>
   );
 };
+
+const styles = StyleSheet.create({
+  listIcon: {
+    backgroundColor: primaryColor,
+    width: 50,
+    height: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 5,
+  },
+  status: {
+    textAlign: 'center',
+    lineHeight: 15,
+    fontWeight: '700',
+  },
+});
 
 export default VDAccountSettingsScreen;
