125e0d7f29976dec3be18b74b7a0205cbff9dee5
kescript <nnamanikester@gmail.com>
2020-06-19 05:54:36 +0100

    Created Apollo file structure

---
+++ src/Apollo/config.js
@@ -0,0 +1,5 @@
+import ApolloClient from 'apollo-boost';
+
+export const client = new ApolloClient({
+  uri: 'https://5d30d83ba92d.ngrok.io',
+});
--- src/App.js
+++ src/App.js
@@ -4,14 +4,18 @@ import { Provider } from 'react-redux';
 import Thunk from 'redux-thunk';
 import reducers from './redux/reducers';
 import NavigationFlows from './NavigationFlows';
+import { ApolloProvider } from '@apollo/react-hooks';
+import { client } from './apollo/config';
 
 const App = () => {
   const store = createStore(reducers, {}, applyMiddleware(Thunk));
 
   return (
-    <Provider store={store}>
-      <NavigationFlows />
-    </Provider>
+    <ApolloProvider client={client}>
+      <Provider store={store}>
+        <NavigationFlows />
+      </Provider>
+    </ApolloProvider>
   );
 };
 
