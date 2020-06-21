7a41fbeb22c398be28c467adc6c3d0353bb2dd4d
kescript <nnamanikester@gmail.com>
2020-06-21 22:06:33 +0100

    Imprted all screens

--- src/screens/CreateProfileScreens/CreateProfileScreen.js
+++ src/screens/CreateProfileScreens/CreateProfileScreen.js
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
 import { Link, Text, Layout } from '../../components/common';
-import CreateProfileInitial from './createProfileInitial';
-import customerStep1 from './customerSteps/customerStep1';
+import CustomerStep1 from './customerSteps/CustomerStep1';
+import CustomerStep2 from './customerSteps/CustomerStep2';
+import VendorStep1 from './vendorSteps/VendorStep1';
+import VendorStep2 from './vendorSteps/VendorStep2';
+import CreateProfileInitial from './CreateProfileInitial';
 
 const CreateProfileScreen = () => {
   const [step, setStep] = useState(0);
