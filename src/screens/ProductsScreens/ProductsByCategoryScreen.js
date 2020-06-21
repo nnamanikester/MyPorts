472989372259d801cd990734ef2cc3da11b45eee
kescript <nnamanikester@gmail.com>
2020-06-21 13:52:06 +0100

    Redesigned the categories screen

--- src/screens/ProductsScreens/ProductsByCategoryScreen.js
+++ src/screens/ProductsScreens/ProductsByCategoryScreen.js
@@ -55,63 +55,50 @@ const ProductsByCategoryScreen = ({ navigation }) => {
             <Text style={styles.title}>Men</Text>
           </View>
           <Spacer />
-          <Row>
-            <Column size="6" style={{ alignItems: 'center' }}>
-              <Product
-                quantity="89"
-                image={female1}
-                name="Water Proof Bag"
-                vendor="Chiomy Styles"
-              />
-            </Column>
-            <Column style={{ alignItems: 'center' }} size="6">
-              <Product
-                quantity="14"
-                image={male1}
-                name="Table Spoon"
-                vendor="Benson Utilities"
-              />
-            </Column>
-            <Column size="6" style={{ alignItems: 'center' }}>
-              <Product
-                image={female2}
-                name="Female belt holder"
-                vendor="Genny Collections"
-                quantity="31"
-              />
-            </Column>
-            <Column size="6" style={{ alignItems: 'center' }}>
-              <Product
-                quantity="45"
-                image={female3}
-                name="Balenciaga Shoe"
-                vendor="Chucks Ventiany"
-              />
-            </Column>
-            <Column size="6" style={{ alignItems: 'center' }}>
-              <Product
-                quantity="15"
-                image={shoe1}
-                name="Adidas Shoe"
-                vendor="Adidas"
-              />
-            </Column>
-            <Column size="6" style={{ alignItems: 'center' }}>
-              <Product
-                quantity="20"
-                image={female3}
-                name="Nike Shoe"
-                vendor="Nike"
-              />
-            </Column>
-            <Column size="6" style={{ alignItems: 'center' }}>
-              <Product
-                quantity="8"
-                image={female3}
-                name="Gucci Bag"
-                vendor="Cossy Viantae"
-              />
-            </Column>
+          <Row style={{ justifyContent: 'space-between' }}>
+            <Product
+              quantity="89"
+              image={female1}
+              name="Water Proof Bag"
+              vendor="Chiomy Styles"
+            />
+
+            <Product
+              quantity="14"
+              image={male1}
+              name="Table Spoon"
+              vendor="Benson Utilities"
+            />
+            <Product
+              image={female2}
+              name="Female belt holder"
+              vendor="Genny Collections"
+              quantity="31"
+            />
+            <Product
+              quantity="45"
+              image={female3}
+              name="Balenciaga Shoe"
+              vendor="Chucks Ventiany"
+            />
+            <Product
+              quantity="15"
+              image={shoe1}
+              name="Adidas Shoe"
+              vendor="Adidas"
+            />
+            <Product
+              quantity="20"
+              image={female3}
+              name="Nike Shoe"
+              vendor="Nike"
+            />
+            <Product
+              quantity="8"
+              image={female3}
+              name="Gucci Bag"
+              vendor="Cossy Viantae"
+            />
           </Row>
         </View>
       </Layout>
