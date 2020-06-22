91708725ba513f5e7a9b35839849b99884b842ce
kescript <nnamanikester@gmail.com>
2020-06-22 09:12:16 +0100

    Created createProile mutations

--- src/Apollo/mutations/createProfile.js
+++ src/Apollo/mutations/createProfile.js
@@ -1,1 +1,57 @@
 import { gql } from 'apollo-boost';
+
+export const UPDATE_ACCOUNT_TYPE = gql`
+  mutation updateAccountType($type: String!) {
+    updateAccountType(type: $type) {
+      id
+    }
+  }
+`;
+
+export const CREATE_WALLET = gql`
+  mutation createWallet($firstName: String!, $lastName: String!) {
+    createWallet(firstName: $firstName, lastName: $lastName) {
+      id
+    }
+  }
+`;
+
+export const CREATE_VENDOR = gql`
+  mutation createVednor(
+    $shopName: String!
+    $email: String!
+    $phone: String!
+    $logo: String
+    $coverPhoto: String
+    $description: String
+  ) {
+    createVendor(
+      name: $shopName
+      email: $email
+      phone: $phone
+      logo: $logo
+      coverPhoto: $coverPhoto
+      description: $description
+    ) {
+      id
+    }
+  }
+`;
+
+export const CREATE_CUSTOMER = gql`
+  mutation createVednor(
+    $firstName: String!
+    $lastName: String!
+    $phone: String
+    $photo: String
+  ) {
+    createVendor(
+      firstName: $firstName
+      lastName: $lastName
+      phone: $phone
+      imageUrl: $photo
+    ) {
+      id
+    }
+  }
+`;
