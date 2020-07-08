55e58e83f987dfcf850ca1be264bb5224f76ed21
kescript <nnamanikester@gmail.com>
2020-07-08 12:51:05 +0100

    Created the mutaion resolver

--- src/Apollo/mutations/address.js
+++ src/Apollo/mutations/address.js
@@ -1,1 +1,49 @@
 import gql from 'graphql-tag';
+
+export const CREATE_ADDRESS = gql`
+  mutation createAddress(
+    $state: String!
+    $city: String
+    $lga: String
+    $phone: String!
+    $address: String!
+    $postalCode: String!
+    $name: String!
+    $customerId: String!
+    $isDefault: Boolean!
+  ) {
+    createAddress(
+      state: $state
+      city: $city
+      lga: $lga
+      phone: $phone
+      address: $address
+      postalCode: $postalCode
+      name: $name
+      customerId: $customerId
+      isDefault: $isDefault
+    ) {
+      id
+      state
+      city
+      lga
+      phone
+      address
+      postalCode
+      name
+      customer {
+        id
+      }
+      default {
+        id
+        address {
+          id
+        }
+        customer {
+          id
+        }
+      }
+      status
+    }
+  }
+`;
