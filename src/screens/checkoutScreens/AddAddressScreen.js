593d66437c2fd7b3b20a21cabf3b7cb00094638e
kescript <nnamanikester@gmail.com>
2020-07-08 13:08:49 +0100

    Created the neccesary state and added it to the text input

--- src/screens/checkoutScreens/AddAddressScreen.js
+++ src/screens/checkoutScreens/AddAddressScreen.js
@@ -1,127 +1,143 @@
 import React, { useState } from 'react';
 import { View, StyleSheet } from 'react-native';
-import {
-  Layout,
-  Icon,
-  Text,
-  Button,
-  TextInput,
-  Spacer,
-  Switch,
-  Clickable,
-  Row,
-} from '../../components/common';
+import * as UI from '../../components/common';
 import Header from '../../components/Header';
 import { useMutation } from '@apollo/react-hooks';
 import { CREATE_ADDRESS } from '../../apollo/mutations';
 
 const AddAddressScreen = ({ navigation }) => {
   const [isDefault, setIsDefault] = useState(false);
+  const [name, setName] = useState('');
+  const [address, setAddress] = useState('');
+  const [state, setState] = useState('');
+  const [city, setCity] = useState('');
+  const [lga, setLga] = useState('');
+  const [postalCode, setPostalCode] = useState('');
+  const [phone, setPhone] = useState('');
 
   return (
     <>
       <Header
         title="Add New Address"
         headerLeft={
-          <Clickable onClick={() => navigation.goBack()}>
-            <Icon name="ios-arrow-back" color="#fff" />
-          </Clickable>
+          <UI.Clickable onClick={() => navigation.goBack()}>
+            <UI.Icon name="ios-arrow-back" color="#fff" />
+          </UI.Clickable>
         }
       />
-      <Layout>
+      <UI.Layout>
         <View style={styles.container}>
           <View>
-            <Text h3>Add A New Shipping Address</Text>
-            <Text note color="">
-              The Fields marked with <Text color="red">*</Text> are required
-            </Text>
+            <UI.Text h3>Add A New Shipping Address</UI.Text>
+            <UI.Text note color="">
+              The Fields marked with <UI.Text color="red">*</UI.Text> are
+              required
+            </UI.Text>
           </View>
-          <Spacer medium />
+          <UI.Spacer medium />
 
           <View style={styles.inputContainer}>
-            <Text heading>
-              Full Name <Text color="red">*</Text>
-            </Text>
-            <Spacer />
-            <TextInput />
+            <UI.Text heading>
+              Full Name <UI.Text color="red">*</UI.Text>
+            </UI.Text>
+            <UI.Spacer />
+            <UI.TextInput
+              value={name}
+              onChangeText={(value) => setName(value)}
+            />
           </View>
 
-          <Spacer medium />
+          <UI.Spacer medium />
           <View style={styles.inputContainer}>
-            <Text heading>
+            <UI.Text heading>
               {' '}
-              Street Address <Text color="red">*</Text>
-            </Text>
-            <Spacer />
-            <TextInput />
+              Street Address <UI.Text color="red">*</UI.Text>
+            </UI.Text>
+            <UI.Spacer />
+            <UI.TextInput
+              value={address}
+              onChangeText={(value) => setAddress(value)}
+            />
           </View>
 
-          <Spacer medium />
+          <UI.Spacer medium />
           <View style={styles.inputContainer}>
-            <Text heading>
-              State <Text color="red">*</Text>
-            </Text>
-            <Spacer />
-            <TextInput />
+            <UI.Text heading>
+              State <UI.Text color="red">*</UI.Text>
+            </UI.Text>
+            <UI.Spacer />
+            <UI.TextInput
+              value={state}
+              onChangeText={(value) => setState(value)}
+            />
           </View>
 
-          <Spacer medium />
+          <UI.Spacer medium />
           <View style={styles.inputContainer}>
-            <Text heading>City</Text>
-            <Spacer />
-            <TextInput />
+            <UI.Text heading>City</UI.Text>
+            <UI.Spacer />
+            <UI.TextInput
+              value={city}
+              onChangeText={(value) => setCity(value)}
+            />
           </View>
 
-          <Spacer medium />
+          <UI.Spacer medium />
           <View style={styles.inputContainer}>
-            <Text heading>Lga</Text>
-            <Spacer />
-            <TextInput />
+            <UI.Text heading>Lga</UI.Text>
+            <UI.Spacer />
+            <UI.TextInput value={lga} onChangeText={(value) => setLga(value)} />
           </View>
 
-          <Spacer medium />
+          <UI.Spacer medium />
           <View style={styles.inputContainer}>
-            <Text heading>
-              Postal Code <Text color="red">*</Text>
-            </Text>
-            <Spacer />
-            <TextInput />
+            <UI.Text heading>
+              Postal Code <UI.Text color="red">*</UI.Text>
+            </UI.Text>
+            <UI.Spacer />
+            <UI.TextInput
+              value={postalCode}
+              onChangeText={(value) => setPostalCode(value)}
+            />
           </View>
 
-          <Spacer medium />
+          <UI.Spacer medium />
           <View style={styles.inputContainer}>
-            <Text heading>
-              Phone <Text color="red">*</Text>
-            </Text>
-            <Spacer />
-            <TextInput />
+            <UI.Text heading>
+              Phone <UI.Text color="red">*</UI.Text>
+            </UI.Text>
+            <UI.Spacer />
+            <UI.TextInput
+              value={phone}
+              onChangeText={(value) => setPhone(value)}
+            />
           </View>
 
-          <Spacer medium />
+          <UI.Spacer medium />
           <View style={styles.inputContainer}>
-            <Row>
-              <Switch
+            <UI.Row>
+              <UI.Switch
                 onChange={() => setIsDefault(!isDefault)}
                 value={isDefault}
                 style={{ alignSelf: 'flex-start' }}
               />
-              <Spacer medium />
-              <Text heading>Set As Default</Text>
-            </Row>
+              <UI.Spacer medium />
+              <UI.Text heading>Set As Default</UI.Text>
+            </UI.Row>
           </View>
 
-          <Spacer medium />
+          <UI.Spacer medium />
           <View>
-            <Row style={{ justifyContent: 'space-between' }}>
-              <Button>
-                <Text color="#fff">Save</Text>
-              </Button>
-            </Row>
+            <UI.Row style={{ justifyContent: 'space-between' }}>
+              <UI.Button>
+                <UI.Text color="#fff">Save</UI.Text>
+              </UI.Button>
+            </UI.Row>
           </View>
 
-          <Spacer medium />
+          <UI.Spacer medium />
         </View>
-      </Layout>
+      </UI.Layout>
     </>
   );
 };
