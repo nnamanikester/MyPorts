0c81f4ed2caa79c3611cc02192b63655a6b769a2
kescript <nnamanikester@gmail.com>
2020-07-10 18:03:15 +0100

    Created Update Address mutation

--- src/Apollo/mutations/address.js
+++ src/Apollo/mutations/address.js
@@ -47,3 +47,53 @@ export const CREATE_ADDRESS = gql`
     }
   }
 `;
+
+export const UPDATE_ADDRESS = gql`
+  mutation updateAddress(
+    $id: String!
+    $state: String!
+    $city: String
+    $lga: String
+    $phone: String!
+    $address: String!
+    $postalCode: String!
+    $name: String!
+    $customerId: String!
+    $isDefault: Boolean!
+  ) {
+    updateAddress(
+      id: $id
+      state: $state
+      city: $city
+      lga: $lga
+      phone: $phone
+      address: $address
+      postalCode: $postalCode
+      name: $name
+      customerId: $customerId
+      isDefault: $isDefault
+    ) {
+      id
+      state
+      city
+      lga
+      phone
+      address
+      postalCode
+      name
+      customer {
+        id
+      }
+      default {
+        id
+        address {
+          id
+        }
+        customer {
+          id
+        }
+      }
+      status
+    }
+  }
+`;
