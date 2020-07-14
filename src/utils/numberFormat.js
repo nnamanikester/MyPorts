714007d2b883133c033215ef5405faf80f5f18a1
kescript <nnamanikester@gmail.com>
2020-07-14 22:02:29 +0100

    Formated the numberFormat Utils

--- src/utils/numberFormat.js
+++ src/utils/numberFormat.js
@@ -12,15 +12,23 @@ export const formatMoney = (amount) => {
 export const formatCardNo = (no) => {
   let acc = '';
   for (let i = 0; i < no.length; i++) {
-    if (i % 4 === 0) acc += '  ';
+    if (i % 4 === 0) {
+      acc += '  ';
+    }
     acc += no[i];
   }
   return acc;
 };
 
 export const formatShortNumber = (num) => {
-  if (Math.abs(num) > 999999999) return Math.sign(num) * ((Math.abs(num) / 1000000000).toFixed(1)) + 'b';
-  if (Math.abs(num) > 999999) return Math.sign(num) * ((Math.abs(num) / 1000000).toFixed(1)) + 'm';
-  if (Math.abs(num) > 999) return Math.sign(num) * ((Math.abs(num) / 1000).toFixed(1)) + 'k';
+  if (Math.abs(num) > 999999999) {
+    return Math.sign(num) * (Math.abs(num) / 1000000000).toFixed(1) + 'b';
+  }
+  if (Math.abs(num) > 999999) {
+    return Math.sign(num) * (Math.abs(num) / 1000000).toFixed(1) + 'm';
+  }
+  if (Math.abs(num) > 999) {
+    return Math.sign(num) * (Math.abs(num) / 1000).toFixed(1) + 'k';
+  }
   return Math.sign(num) * Math.abs(num);
-}
+};
