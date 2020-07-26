0751f0d45ba42212d9112f9e2f632840bdf24037
kescript <nnamanikester@gmail.com>
2020-07-26 22:56:13 +0100

    Created resolvers for cart item

--- src/Apollo/mutations/cart.js
+++ src/Apollo/mutations/cart.js
@@ -82,3 +82,153 @@ export const ADD_ITEM_TO_CART = gql`
     }
   }
 `;
+
+export const REMOVE_CART_ITEM = gql`
+  mutation removeCartItem($cartId: String!, $itemId: String!) {
+    removeCartItem(itemId: $itemId, cartId: $cartId) {
+      id
+      customer {
+        id
+      }
+      items {
+        id
+        quantity
+        product {
+          id
+          images {
+            url
+          }
+          name
+          createdAt
+          price
+          category {
+            name
+          }
+          vendor {
+            id
+            profile {
+              id
+              name
+            }
+          }
+          description
+          quantity
+          shipping
+          specifications {
+            value
+            specification
+          }
+          fixedDiscount
+          percentageDiscount
+          status
+          likes {
+            id
+            customer {
+              id
+            }
+          }
+          comments {
+            id
+            comment
+            customer {
+              id
+              firstName
+              lastName
+              photo
+            }
+            createdAt
+            updatedAt
+          }
+          saves {
+            id
+            customer {
+              id
+            }
+          }
+          shares {
+            id
+          }
+          quantity
+          createdAt
+        }
+      }
+      createdAt
+      updatedAt
+    }
+  }
+`;
+
+export const CLEAR_CART = gql`
+  mutation clearCart($id: String!) {
+    clearCartItem(id: $id) {
+      id
+      customer {
+        id
+      }
+      items {
+        id
+        quantity
+        product {
+          id
+          images {
+            url
+          }
+          name
+          createdAt
+          price
+          category {
+            name
+          }
+          vendor {
+            id
+            profile {
+              id
+              name
+            }
+          }
+          description
+          quantity
+          shipping
+          specifications {
+            value
+            specification
+          }
+          fixedDiscount
+          percentageDiscount
+          status
+          likes {
+            id
+            customer {
+              id
+            }
+          }
+          comments {
+            id
+            comment
+            customer {
+              id
+              firstName
+              lastName
+              photo
+            }
+            createdAt
+            updatedAt
+          }
+          saves {
+            id
+            customer {
+              id
+            }
+          }
+          shares {
+            id
+          }
+          quantity
+          createdAt
+        }
+      }
+      createdAt
+      updatedAt
+    }
+  }
+`;
