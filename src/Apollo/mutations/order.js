6c90875abb1c92f08e51e3a7501a75152b0a7670
kescript <nnamanikester@gmail.com>
2020-08-31 23:37:31 +0100

    Order status update working

--- src/Apollo/mutations/order.js
+++ src/Apollo/mutations/order.js
@@ -1,88 +1,105 @@
 import gql from 'graphql-tag';
 
 export const UPDATE_ORDER = gql`
-  mutation updateOrder($id: String!, $data: OrderUpdateInput!) {
+  mutation updateOrder($id: String!, $data: OrderItemUpdateInput!) {
     updateOrder(id: $id, data: $data) {
       id
-      orderNo
-      status
-      items {
+      product {
         id
-        address {
-          id
+        images {
+          url
         }
-        amount
-        quantity
-        product {
-          id
-          images {
-            url
-          }
+        name
+        createdAt
+        price
+        category {
           name
-          createdAt
-          price
-          category {
-            name
-            id
-          }
-          vendor {
-            id
-            profile {
-              id
-              name
-            }
-          }
-          description
-          quantity
-          shipping
-          specifications {
-            value
-            specification
-          }
-          fixedDiscount
-          percentageDiscount
-          status
-          likes {
-            id
-            customer {
-              id
-            }
-          }
-          comments {
-            id
-            comment
-            customer {
-              id
-              firstName
-              lastName
-              photo
-            }
-            createdAt
-            updatedAt
-          }
-          saves {
-            id
-            customer {
-              id
-            }
-          }
-          shares {
-            id
-          }
-        }
-        customer {
           id
         }
         vendor {
           id
+          profile {
+            id
+            name
+          }
         }
-        order {
+        description
+        quantity
+        shipping
+        specifications {
+          value
+          specification
+        }
+        fixedDiscount
+        percentageDiscount
+        status
+        likes {
+          id
+          customer {
+            id
+          }
+        }
+        comments {
+          id
+          comment
+          customer {
+            id
+            firstName
+            lastName
+            photo
+          }
+          createdAt
+          updatedAt
+        }
+        saves {
+          id
+          customer {
+            id
+          }
+        }
+        shares {
           id
         }
+      }
+      quantity
+      order {
+        id
+        orderNo
         status
         createdAt
         updatedAt
       }
+      customer {
+        id
+      }
+      vendor {
+        id
+      }
+      amount
+      address {
+        id
+        state
+        city
+        lga
+        phone
+        address
+        postalCode
+        name
+        customer {
+          id
+        }
+        default {
+          id
+          address {
+            id
+          }
+          customer {
+            id
+          }
+        }
+        status
+      }
+      status
       createdAt
       updatedAt
     }
--- src/screens/VDScreens/VDOrderDetailsScreen.js
+++ src/screens/VDScreens/VDOrderDetailsScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {StyleSheet, View} from 'react-native';
+import {StyleSheet, View, Alert, ToastAndroid} from 'react-native';
 import * as UI from '../../components/common';
 import Header from '../../components/Header';
 import CartItem from '../../components/CartItem';
@@ -7,11 +7,119 @@ import {connect} from 'react-redux';
 import moment from 'moment';
 import {formatMoney} from '../../utils';
 import {info} from '../../components/common/variables';
+import {UPDATE_ORDER} from '../../apollo/mutations';
+import {useMutation} from '@apollo/react-hooks';
+import {setVendorOrders} from '../../redux/actions/OrderActions';
+const VDOrderDetailsScreen = ({navigation, route: {params}, orders}) => {
+  const [order, setOrder] = React.useState(params.order);
+  const [confirmAccept, setConfirmAccept] = React.useState(false);
+  const [confirmDecline, setConfirmDecline] = React.useState(false);
+  const [confirmDelivery, setConfirmDelivery] = React.useState(false);
 
-const VDOrderDetailsScreen = ({navigation, route: {params}}) => {
-  const {order} = params;
+  const [updateOrder, {loading}] = useMutation(UPDATE_ORDER);
+
+  const handleAcceptOrder = () => {
+    setConfirmAccept(false);
+    updateOrder({
+      variables: {
+        id: order.id,
+        data: {
+          status: 2,
+        },
+      },
+    })
+      .then((res) => {
+        setVendorOrders(
+          orders.map((o) => {
+            if (o.id === order.id) {
+              return res.data.updateOrder;
+            }
+            return o;
+          }),
+        );
+        setOrder(res.data.updateOrder);
+        ToastAndroid.show(
+          'Order status have been updated successfully!',
+          ToastAndroid.LONG,
+        );
+      })
+      .catch(() => {
+        Alert.alert(
+          'Error!',
+          'Unable to Accept Order. Please check your internet connection and try again!',
+        );
+      });
+  };
+
+  const handleDeclineOrder = () => {
+    setConfirmDecline(false);
+    updateOrder({
+      variables: {
+        id: order.id,
+        data: {
+          status: 0,
+        },
+      },
+    })
+      .then((res) => {
+        setVendorOrders(
+          orders.map((o) => {
+            if (o.id === order.id) {
+              return res.data.updateOrder;
+            }
+            return o;
+          }),
+        );
+        setOrder(res.data.updateOrder);
+        ToastAndroid.show(
+          'Order status have been updated successfully!',
+          ToastAndroid.LONG,
+        );
+      })
+      .catch(() => {
+        Alert.alert(
+          'Error!',
+          'Unable to Decline Order. Please check your internet connection and try again!',
+        );
+      });
+  };
+
+  const handleOrderDelivery = () => {
+    setConfirmDelivery(false);
+    updateOrder({
+      variables: {
+        id: order.id,
+        data: {
+          status: 3,
+        },
+      },
+    })
+      .then((res) => {
+        setVendorOrders(
+          orders.map((o) => {
+            if (o.id === order.id) {
+              return res.data.updateOrder;
+            }
+            return o;
+          }),
+        );
+        setOrder(res.data.updateOrder);
+        ToastAndroid.show(
+          'Order status have been updated successfully!',
+          ToastAndroid.LONG,
+        );
+      })
+      .catch(() => {
+        Alert.alert(
+          'Error!',
+          'Unable to mark  order as delivered. Please check your internet connection and try again!',
+        );
+      });
+  };
+
   return (
     <>
+      <UI.Loading show={loading} />
       <Header
         title="Order Details"
         headerLeft={
@@ -82,7 +190,7 @@ const VDOrderDetailsScreen = ({navigation, route: {params}}) => {
 
         <View style={styles.container}>
           {order.status === 1 ? (
-            <UI.Button>
+            <UI.Button onClick={() => setConfirmAccept(true)}>
               <UI.Text color="#fff">Accept</UI.Text>
             </UI.Button>
           ) : null}
@@ -90,11 +198,13 @@ const VDOrderDetailsScreen = ({navigation, route: {params}}) => {
           <UI.Spacer />
 
           {order.status === 1 ? (
-            <UI.Button type="ghost">Decline</UI.Button>
+            <UI.Button onClick={() => setConfirmDecline(true)} type="ghost">
+              Decline
+            </UI.Button>
           ) : null}
 
           {order.status === 2 ? (
-            <UI.Button>
+            <UI.Button onClick={() => setConfirmDelivery(true)}>
               <UI.Text color="#fff">Mark a delivered</UI.Text>
             </UI.Button>
           ) : null}
@@ -109,6 +219,73 @@ const VDOrderDetailsScreen = ({navigation, route: {params}}) => {
           <UI.Spacer large />
         </View>
       </UI.Layout>
+      <UI.Modal show={confirmAccept}>
+        <UI.Spacer medium />
+
+        <UI.Text size={18}>Are you sure you want to accept this order?</UI.Text>
+
+        <UI.Spacer medium />
+
+        <UI.Row
+          style={{justifyContent: 'space-between', paddingHorizontal: 30}}>
+          <UI.Button
+            size="small"
+            onClick={() => setConfirmAccept(false)}
+            type="ghost">
+            Cancel
+          </UI.Button>
+          <UI.Button size="small" onClick={handleAcceptOrder}>
+            <UI.Text color="#fff">Accept</UI.Text>
+          </UI.Button>
+        </UI.Row>
+      </UI.Modal>
+
+      <UI.Modal show={confirmDecline}>
+        <UI.Spacer medium />
+
+        <UI.Text size={18}>
+          Are you sure you want to Decline this order?
+        </UI.Text>
+
+        <UI.Spacer medium />
+
+        <UI.Row
+          style={{justifyContent: 'space-between', paddingHorizontal: 30}}>
+          <UI.Button
+            size="small"
+            onClick={() => setConfirmDecline(false)}
+            type="ghost">
+            Cancel
+          </UI.Button>
+          <UI.Button size="small" onClick={handleDeclineOrder}>
+            <UI.Text color="#fff">Decline</UI.Text>
+          </UI.Button>
+        </UI.Row>
+      </UI.Modal>
+
+      <UI.Modal show={confirmDelivery}>
+        <UI.Spacer medium />
+
+        <UI.Text style={{textAlign: 'center'}}>
+          Are you sure this order have been delivered to the buyer? {'\n'}
+          NB: We will have to confirm your delivery before you receive your
+          payment.
+        </UI.Text>
+
+        <UI.Spacer medium />
+
+        <UI.Row style={{justifyContent: 'space-between'}}>
+          <UI.Button
+            size="small"
+            onClick={() => setConfirmDelivery(false)}
+            type="ghost">
+            Cancel
+          </UI.Button>
+          <UI.Button style={{width: 200}} onClick={handleOrderDelivery}>
+            <UI.Text color="#fff">Mark as delivered</UI.Text>
+          </UI.Button>
+        </UI.Row>
+      </UI.Modal>
     </>
   );
 };
@@ -129,7 +306,7 @@ const styles = StyleSheet.create({
 
 const mapStateToProps = (state) => {
   return {
-    orders: state.orders,
+    orders: state.orders.vendorOrders,
   };
 };
 
