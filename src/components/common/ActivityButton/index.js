96fa2e97f972abc7831d31b5c9b53694e131507c
kescript <nnamanikester@gmail.com>
2020-09-06 03:53:06 +0100

    feat: added animation to activity button

--- src/components/common/ActivityButton/index.js
+++ src/components/common/ActivityButton/index.js
@@ -1,16 +1,40 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import {View, StyleSheet} from 'react-native';
+import {
+  View,
+  StyleSheet,
+  Platform,
+  UIManager,
+  LayoutAnimation,
+} from 'react-native';
 import {Text} from '../Text';
-import {Row} from '../Row';
 import {Spacer} from '../Spacer';
 import {info} from '../variables';
 import {Clickable} from '../Clickable';
 
+if (
+  Platform.OS === 'android' &&
+  UIManager.setLayoutAnimationEnabledExperimental
+) {
+  UIManager.setLayoutAnimationEnabledExperimental(true);
+}
+
 const ActivityButton = ({activeIcon, inActiveIcon, onClick, count}) => {
+  const [resize, setResize] = React.useState(24);
+
+  React.useMemo(() => {
+    LayoutAnimation.spring();
+  }, [onClick]);
+
   return (
     <View style={styles.container}>
-      <Clickable onClick={onClick}>{activeIcon || inActiveIcon}</Clickable>
+      <Clickable
+        onPressIn={() => setResize(16)}
+        onPressOut={() => setResize(24)}
+        style={{width: resize, height: resize}}
+        onClick={onClick}>
+        {activeIcon || inActiveIcon}
+      </Clickable>
       <Spacer />
       <View style={styles.iconContainer}>
         <Text size={14} color={info}>
